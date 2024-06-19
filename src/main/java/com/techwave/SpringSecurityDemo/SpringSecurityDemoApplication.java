208c68da5992f699738435c8ad4e1884952a5dab
varalakshmi.t <varalakshmi.t@TWINH-DES-0318.in.techwave.net>
2024-06-19 15:31:50 +0530

    Used the H2 database in server mode

--- src/main/java/com/techwave/SpringSecurityDemo/SpringSecurityDemoApplication.java
+++ src/main/java/com/techwave/SpringSecurityDemo/SpringSecurityDemoApplication.java
@@ -1,6 +1,10 @@
 package com.techwave.SpringSecurityDemo;
 
 import org.springframework.boot.SpringApplication;
+
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,7 +13,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class SpringSecurityDemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		Server.createTcpServer().start();
 		SpringApplication.run(SpringSecurityDemoApplication.class, args);
 	}
 
